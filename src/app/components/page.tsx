c3e0ecc9950a1265656a1b6eae761bba64a73c38
Oscar Bonelli <bonelli.personal@gmail.com>
2025-09-01 08:00:08 -0600

    feature: readme v3

---
+++ src/app/components/page.tsx
@@ -0,0 +1,9 @@
+// src/app/components/page.tsx
+export default function Page() {
+    return (
+        <div>
+            <h1>Hello from Page.tsx 👋</h1>
+            <p>This is just a placeholder component.</p>
+        </div>
+    );
+}
