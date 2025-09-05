03f6ddfa6a23a63967e782788d86e8aeaa4d19d9
Oscar Bonelli <bonelli.personal@gmail.com>
2025-09-05 12:48:27 -0600

    feature: phone icon update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,14 @@ export default function HomePage() {
               <path fill="currentColor" d="M22 6v12H2V6l10 7L22 6Zm-10 5L2 6h20l-10 5Z" />
             </IconLink>
 
+            {/* Teléfono */}
+            <IconLink href="tel:+528332087965" label="+52 833 208 79 65">
+              <path
+                fill="currentColor"
+                d="M6.62 10.79a15.05 15.05 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.01-.24c1.12.37 2.33.57 3.58.57a1 1 0 0 1 1 1V21a1 1 0 0 1-1 1C10.07 22 2 13.93 2 3a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.46.57 3.58a1 1 0 0 1-.24 1.01l-2.21 2.2Z"
+              />
+            </IconLink>
+
             <IconLink href="https://www.linkedin.com/in/obonelli" label="LinkedIn">
               <path
                 fill="currentColor"
